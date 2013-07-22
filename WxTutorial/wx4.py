e2f4bf8743494fa35df80952d19a8607f9f926a0
Jeffrey Killeen <jtk12b@acu.edu>
2013-07-22 18:28:08 -0500

    made changes to wx4 file

--- WxTutorial/wx4.py
+++ WxTutorial/wx4.py
@@ -6,7 +6,9 @@ class jeff(wx.Frame):
 		wx.Frame.__init__(self, parent, id, 'Frame aka window', size=(300,200))
 		panel=wx.Panel(self)
 		
-		
+		box=wx.MessageDialog(None, 'Do you like waffles?','Title',wx.YES_NO)
+		answer = box.ShowModal()
+		box.Destroy()
 	
 if __name__=='__main__':
 	app=wx.PySimpleApp()
